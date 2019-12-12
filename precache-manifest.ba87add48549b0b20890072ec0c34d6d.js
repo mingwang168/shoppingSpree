18d818bc9b7f8f1f485903e02798a1c29f17b414
mingwang168 <mingwang0904@gmail.com>
2019-12-12 12:53:35 -0800

    'first_commit'

---
+++ precache-manifest.ba87add48549b0b20890072ec0c34d6d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fe26160c1999e0124a4c10e6aa9db763",
+    "url": "./index.html"
+  },
+  {
+    "revision": "34de111006d5c40e91d6",
+    "url": "./static/css/main.078560b2.chunk.css"
+  },
+  {
+    "revision": "8af35852152cdd588391",
+    "url": "./static/js/2.a22ef601.chunk.js"
+  },
+  {
+    "revision": "34de111006d5c40e91d6",
+    "url": "./static/js/main.1fc35b82.chunk.js"
+  },
+  {
+    "revision": "56a6b2399647f3d207bd",
+    "url": "./static/js/runtime-main.f1c4a8af.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "./static/media/logo.25bf045c.svg"
+  }
+]);
